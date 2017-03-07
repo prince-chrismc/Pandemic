c67f1b0f0c76e739d2251b088874ec655c4098e9
chris mcarthur <prince.chrismc@gmail.com>
2017-03-07 13:47:03 -0500

    Basic board (needs development)

---
+++ Pandemic/Sources/Board.cpp
@@ -0,0 +1,54 @@
+#include <algorithm> //std::shuffle
+#include <random> //std::mt19937
+#include "Board.h"
+
+void Board::InfectCity(const uint8_t& cubesToAdd)
+{
+	InfectionCard* ic = m_InfecDeck.DrawCard();
+	City::CityID cid = (City::CityID)(ic->getNumID() - InfectionCard::INFECTIONCARD_MIN);
+	delete ic; ic = nullptr;
+
+	City* city = m_Map.getCityWithID(cid);
+
+	for(uint8_t i = 0; i < cubesToAdd; i += 1)
+		city->addCube(m_Cubes.takeCube(city->getCityColor()));
+}
+
+void Board::Outbreak(City* city)
+{
+	m_OutBreak.IncreaseRate();
+
+	for each(City* connected in city->getNearByCities())
+	{
+		connected->addCube(m_Cubes.takeCube(city->getCityColor()));
+	}
+}
+
+void Board::Epidemic()
+{
+	// 1. Increase Infection Rate
+	m_InfectRate.IncreaseRate();
+
+	// 2. Infect Last Card of InfectionDeck with at most 3 Cubes
+	InfectionCard* ic = m_InfecDeck.DrawCardForEpidemic();
+	Color c = ic->getCityColor();
+	if(m_Cures.IsNotEradicated(c));
+	{
+		City::CityID cid = (City::CityID)(ic->getNumID() - InfectionCard::INFECTIONCARD_MIN);
+		delete ic; ic = nullptr;
+
+		City* city = m_Map.getCityWithID(cid);
+
+		for (uint8_t i = city->GetNumberOfCubes(); i < 3; i += 1)
+			city->addCube(m_Cubes.takeCube(city->getCityColor()));
+
+		// 2.1. Otbreaks if Need be
+		if (city->GetNumberOfCubes() > 0)
+		{
+			Outbreak(city);
+		}
+
+		// 3. Intensify reshuffle infect discard and add on top of deck
+		m_InfecDeck.Intensify();
+	}
+}
---
+++ Pandemic/Sources/Board.h
@@ -0,0 +1,40 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Board Definition for the Pandemic Game
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "Deck.h"
+#include "Token.h"
+#include "WorldMap.h"
+
+class Board
+{
+	friend class GameEngine;
+private:
+	WorldMap m_Map;
+	InfectionRate m_InfectRate;
+	OutbreakMarker m_OutBreak;
+	DiseaseCubePile m_Cubes;
+	InfectionDeck m_InfecDeck;
+	PlayerDeck m_PlayerDeck;
+	RoleDeck m_RoleDeck;
+	CureMakers m_Cures;
+	ResearchStations m_Centers;
+
+protected:
+
+public:
+	Board() : m_Map(), m_InfectRate(), m_OutBreak(), m_Cubes(), m_InfecDeck(), m_PlayerDeck(), m_RoleDeck(), m_Cures(), m_Centers()  {}
+	~Board() {}
+
+	void InfectCity(const uint8_t& cubesToAdd = 1); // Draw infection card and infect city with X cubes of its color
+	
+	void Outbreak(City* city); //add one cube of that city's color to all connected cities
+	void Epidemic(); //Increase, Infect, Intensify
+	void DrawPlayerCard() { m_PlayerDeck.DrawCard(); }
+	void DiscardPlayerCard(PlayerCard* pc) { m_PlayerDeck.DiscardCard(pc); }
+
+};
+
