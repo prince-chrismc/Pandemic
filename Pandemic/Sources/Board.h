10fa465d4330a00412cad3155f8c95fe8ebc0535
chris mcarthur <prince.chrismc@gmail.com>
2017-04-17 22:26:29 -0400

    removed heap allocation

--- Pandemic/Sources/Board.h
+++ Pandemic/Sources/Board.h
@@ -14,20 +14,20 @@ class Board final // Container Class
 {
 	friend class GameEngine;
 private:
-	WorldMap* m_Map;
+	WorldMap m_Map;
 	InfectionRate m_InfectRate;
 	OutbreakMarker m_OutBreak;
 	DiseaseCubePile m_Cubes;
 	InfectionDeck m_InfecDeck;
 	PlayerDeck m_PlayerDeck;
 	RoleDeck m_RoleDeck;
-	CureMakers* m_Cures;
-	ResearchStations* m_Centers;
-	WorldObserver* m_WorldObserver;
-	CureObserver* m_CureObserver;
+	CureMarkers m_Cures;
+	ResearchStations m_Centers;
+	WorldObserver m_WorldObserver;
+	CureObserver m_CureObserver;
 	
 public:
-	Board() : m_Map(new WorldMap()), m_InfectRate(), m_OutBreak(), m_Cubes(), m_InfecDeck(), m_PlayerDeck(), m_RoleDeck(), m_Cures(new CureMakers()), m_Centers(new ResearchStations()), m_WorldObserver(new WorldObserver(m_Centers, m_Map)), m_CureObserver(new CureObserver(m_Cures)) { m_Map->RegistarObserver(m_WorldObserver); m_Centers->RegistarObserver(m_WorldObserver); m_Cures->RegistarObserver(m_CureObserver); }
+	Board() : m_Map(), m_InfectRate(), m_OutBreak(), m_Cubes(), m_InfecDeck(), m_PlayerDeck(), m_RoleDeck(), m_Cures(), m_Centers(), m_WorldObserver(&m_Centers, &m_Map), m_CureObserver(&m_Cures) { m_Map.MapSubject::RegistarObserver(&m_WorldObserver); m_Centers.StationsSubject::RegistarObserver(&m_WorldObserver); m_Cures.RegistarObserver(&m_CureObserver); }
 	~Board() {}
 
 	///Prevent Copy/Assignment
